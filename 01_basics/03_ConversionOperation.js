5e272ac6809e269c8141d6bdd532b1d486c75331
nehav2027 <nehakumbhar2478@gmail.com>
2026-05-06 07:21:08 +0000

    done

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -36,4 +36,18 @@ let negvalue = -value
 //console.log(2**2);
 //console.log(2%2);
 
+let str1= "Hello"
+let str2 =" Viha"
+
+let str3 = str1+str2
+//console.log(str3);
+
+
+//console.log(1 + "3");
+//console.log("1"+ 3);
+//console.log("1" + "3");
+//console.log(1 + 2 + " 3");
+
+
+
 
