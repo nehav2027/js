adf6286245b2426538a84bd91e5febf2082a13cc
nehav2027 <nehakumbhar2478@gmail.com>
2026-05-06 10:32:41 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+// console.log("2">=1); // CONVERSION HAPPENS
+// console.log("02">=1);
+
+// console.log(null>0);
+// console.log(null==0);    // AVOID ALL THESE 3 TYPES OF CONVERTION
+// console.log(null<=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);     // AVOID ALL THESE 3 TYPES OF CONVERTION
+// console.log(undefined<0);
+
+// STRICT CHECK ===
+
+console.log("2"===2);
+
+
+
+
---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,45 @@
+ 
+/* JAVASCRIPT IS DYNAMICALLY TYPED LANGUAGE
+ For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
+ 
+ PRIMITIVE DATATYPE :7types
+string number boolean null undefined symbol BigInt
+
+const score= 100
+const outsidetemp= null
+let userEmail;
+
+SYMBOLS
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id===anotherid);
+
+const bignumber= 234567897654324567897654345678n
+
+
+
+
+ NON PRIMITIVE DATATYPE (REFERENCE TYPE)
+
+ ARRAY & OBJECT & FUNCTION
+
+ array 
+ const heros =["Spiderman", "Batman", "Captain America"]
+
+ Object (Written in KEY VALUE PAIR)
+ let myobj = {
+ name: "Neha"
+ age: 20
+}
+
+function */
+
+const myfunction  = function(){
+    console.log("Hello World!");
+    
+}
