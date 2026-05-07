b8d4fc1821293333b065fbdfa4d287a7101ce451
neha <nehakumbhar2478@gmail.com>
2026-05-07 15:59:44 +0530

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,50 @@
+/*An array is used to store multiple values in a single variable.
+ORDERED, INDEXED AND MUTABLE
+
+const myarray = [0,1,2,3,4,5, true, "Neha"]
+
+console.log(myarray[0]);
+
+const myheros = ["Spiderman", "Batman"]
+
+const myarr = new Array(0,1,2,3,4,5)
+console.log(myarr[1]);
+
+METHODS IN ARRAY
+
+console.log(myarr.push(6))
+
+console.log(myarr.pop())
+
+console.log(myarr.unshift(4));
+
+console.log(myarr.shift());
+
+const newArr= myarr.join()
+console.log(myarr);
+console.log(newArr);
+
+
+*********************************************************************************************************
+SLICE AND SPLICE
+
+console.log("A", myarr);
+
+
+const myn1 =myarr.slice(1,3)
+console.log("B",myarr);
+
+const myn2 = myarr.splice(1,3)
+console.log(myn1);
+console.log(myn2);*/
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,27 @@
+/*const marvel_heros= ["Spiderman", "Ironman",]
+console.log(marvel_heros);
+
+const dc_heros=["Batman","Superman"]
+console.log(dc_heros);
+
+const all_heros= marvel_heros.concat(dc_heros) //CONCATENATION
+console.log(all_heros);
+
+const all_new_heros=[...marvel_heros,...dc_heros]
+console.log(all_new_heros);
+
+const another_array=[1,2,[2,4],3,4,5,6,[6,4,5,7]]
+const real_another_array= another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.from({name : "Neha"}));    //Interesting 
+console.log(Array.from("Neha"));  
+console.log(Array.isArray({name : "Neha"}));  
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));
+*/
+
+
