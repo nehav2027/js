a2c6f09ca4c057d771f34821d2a23970f6584ddc
neha <nehakumbhar2478@gmail.com>
2026-05-07 13:18:31 +0530

    done

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,25 @@
+// let mydate = new Date
+// console.log(mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate);
+
+// let mydate = new Date()
+// console.log(mydate);
+
+let mycreateddate = new Date(2023, 0,5)
+// console.log(mycreateddate.toDateString());
+
+// let mycreateddate = new Date("01-01-2026")
+
+let mytimestamp= Date.now()
+console.log(mytimestamp);
+console.log(mycreateddate.getTime());
+
+
+
+
+
