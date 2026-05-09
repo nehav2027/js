4d3c01f199d7e57351ce86d50ae2b79014c3e762
neha <nehakumbhar2478@gmail.com>
2026-05-09 11:28:02 +0530

    done

---
+++ 02_basics/03_object.js
@@ -0,0 +1,43 @@
+// objects can be declared as a literal or as a constructor
+
+// SINGLETON (Through constructor)
+// Object.create
+
+// OBJECT LITERALS
+/*const mysymbol = Symbol("Key1")
+const jsuser ={
+name: "Neha",
+age: 18,
+location: "Mumbai",
+mailid: "neha@google.in",
+isloggedid: false,
+lastloginday: ["Monday", "Saturday"],
+[mysymbol]:"myKey1"
+}
+
+console.log(jsuser.mailid);
+console.log(jsuser["mailid"]);
+console.log(jsuser[mysymbol]);
+
+jsuser.mailid= "neha@chatgpt.com" // Changing value of mailid
+
+// Object.freeze(jsuser);
+jsuser.mailid = "neha@microsoft.com"
+
+console.log(jsuser);
+
+jsuser.greeting= function(){
+    console.log("Hello JS User");
+    }
+
+    console.log(jsuser.greeting());
+
+jsuser.greetingtwo= function(){
+    console.log(`Hello JS User, ${this.name}`);
+    }    
+    
+    console.log(jsuser.greetingtwo());
+*/
+
+
+
---
+++ 02_basics/04_object.js
@@ -0,0 +1,84 @@
+// const tinderuser = new Object()
+
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "sam"
+tinderuser.isloggedin = false
+
+// console.log(tinderuser);
+
+const regularuser ={
+    mailid: "sam@mail.in",
+    fullname:{
+        userfullname:{
+            firstname: "Neha",
+            lastname: "Kumbhar"
+        }
+    }
+}
+// console.log(regularuser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+const obj4 = {5:"c", 6:"d"}
+
+// const obj3 ={obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3= {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+const users =[
+    {
+        id: 1,
+        email: "neha@google.in"
+    },
+     {
+        id: 2,
+        email: "nehaa@google.in"
+    },
+     {
+        id: 3,
+        email: "nehaaa@google.in"
+    },
+     {
+        id: 4,
+        email: "nehaaaa@google.in"
+    }
+]
+
+// users[1].email
+// console.log(tinderuser);
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+
+// console.log(tinderuser.hasOwnProperty('isloggedin'));
+
+const course={
+    coursename: "JS IN HINDI",
+    courseprice: 299,
+    courseinstructor: "Hitesh "
+}
+
+//course.courseinstructor
+
+const {courseinstructor: instructor} = course
+console.log(courseinstructor);
+
+
+
+{
+    "coursename": "JS IN HINDI",
+    "courseprice": 299,
+    "courseinstructor": "Hitesh "
+}
+
+[
+    {},
+    {},
+    {}
+]
+
