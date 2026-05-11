248477bba211a31b820e4bddddd12a754f107096
neha <nehakumbhar2478@gmail.com>
2026-05-11 10:30:12 +0530

    done

---
+++ 03_basics/05_execution_context.js
@@ -0,0 +1,41 @@
+/*
+// JAVASCRIPT EXECUTION CONTEXT
+
+    > Global  Execution Context (step 1)
+    this
+
+
+
+
+    > Function Execution Context
+
+
+
+
+    > Eval Execution Context
+
+
+
+    memory creation phase = sirf memory allocate hoti (Step Two)
+val1 undefined
+val2 undefined
+addnum defination 
+result1 undefined
+result2 undefined
+
+    Execution Phase (Step 3)
+val1 10
+val2 5
+addnum = new variable environment + Exeecution Thread
+deletes after execution
+
+memory phase
+val1 undefined
+val2 undefined
+total undefined
+
+execution Context
+num1 10
+num2 5
+total 15
+*/
---
+++ 04_controlflow/one.js
@@ -0,0 +1,64 @@
+/*
+// if
+
+const isuserloggedin = true
+
+if (2 === '2') {
+   console.log("executed");
+    
+}
+
+const temperature = 41
+if (temperature<50) {
+   console.log("Temperature is less than 50");
+    
+}
+else{
+console.log("Temperature is greater than 50");
+}
+
+const score =200
+if (score>100) {
+    const power = "Fly"
+    console.log(`User Power: ${power}`);
+    
+}
+
+
+const balance =1000
+//if (balance>500) console.log("test");  // implict scope
+
+if (balance>500) {
+    
+    console.log("Less than 500");   
+}   
+else if(balance<750)
+{
+    console.log("Less than 750"); 
+}   
+else if( balance<900)
+{
+console.log("Less than 900");
+}
+else{
+    console.log("Less than 1200");
+    
+}
+
+
+const userloggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if (userloggedin && debitcard && 2==3) {
+    console.log("Allow to buy course");
+    
+}
+
+if (loggedinfromgoogle || loggedinfromemail || true ) {
+    console.log("User logged in");
+    
+}
+
+*/
