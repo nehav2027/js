a60577277242fc03bd94b159c4d205527ad0fd15
neha <nehakumbhar2478@gmail.com>
2026-05-11 14:53:46 +0530

    done

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+let index = 0 
+while (index<=10) {
+    console.log(`Value of index is: ${index}`);
+    index = index + 2
+}
+
+let myarray = ['superman', 'batman', 'Spiderman']
+
+let arr = 0
+while (arr < myarray.length) {
+    console.log(`Value is: ${myarray[arr]}`);
+    arr = arr + 1
+    
+}
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
