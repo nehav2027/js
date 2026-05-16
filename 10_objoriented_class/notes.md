233301fba7250cd010b797089a1cca96dcaa56af
Neha <nehakumbhar2478@gmail.com>
2026-05-16 11:43:16 +0530

    done

---
+++ 10_objoriented_class/notes.md
@@ -0,0 +1,13 @@
+#javascript and classes
+
+#OOP
+
+#Object - collection of properties and methods
+-toLowerCase
+
+#why use oop
+Object literal
+- Constructor function
+- Prototype
+- Classes
+- Instances : new keyword, this keyword
---
+++ 10_objoriented_class/oop.js
@@ -0,0 +1,29 @@
+const user = {
+    username:'neha',
+    logincount: 8,
+    signedin: true,
+
+
+    getuserdetails : function(){
+        //console.log("Got user details from database");
+        console.log(`username: ${this.username}`);
+        
+        
+
+    }
+
+}
+//console.log(user.username);
+console.log(user.getuserdetails());
+
+function User (username, logincount,isloggedin){
+    this.username = username
+    this.logincount = logincount
+    this.isloggedin = isloggedin
+
+    return this
+
+}
+
+const userOne = User("Neha", 10, true)
+
